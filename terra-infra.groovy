a3e74e19b683ff1342ee85779faed5ef19b387fe
AnuragKawale14 <129189815+AnuragKawale14@users.noreply.github.com>
2026-02-06 14:09:56 +0530

    Update terra-infra.groovy

--- terra-infra.groovy
+++ terra-infra.groovy
@@ -10,7 +10,7 @@ pipeline {
             steps {
              sh '''cd terraform/eks
                    terraform init
-                    terraform apply --auto-approve'''
+                    terraform plan'''
             }
         }
         stage('Deploy') {
